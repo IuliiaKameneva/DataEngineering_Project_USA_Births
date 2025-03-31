6622bcaf7171bd733db8a962c65e31b3cd77de2d
Iuliia Kameneva <68774955+IuliiaKameneva@users.noreply.github.com>
2025-03-31 19:51:38 +0000

    Add partitions in some tables by years

--- dbt/models/core/fct_county_natality.sql
+++ dbt/models/core/fct_county_natality.sql
@@ -1,7 +1,17 @@
-{{ config(materialized='table') }}
+{{
+  config(
+    materialized='table',
+    partition_by={
+      "field": "year_date", 
+      "data_type": "date",
+      "granularity": "year"
+    }
+  )
+}}
 
 WITH county_natality_year as (
     SELECT 
+        year_date,
         year,
         SUM(births) as births,
         SUM(births * ave_age_of_mother)/sum(births) AS mother_age, 
@@ -11,7 +21,7 @@ WITH county_natality_year as (
         SUM(births * ave_pre_pregnancy_bmi)/sum(births) AS pre_pregnancy_bmi, 
         SUM(births * ave_number_of_prenatal_wks)/sum(births) AS number_of_prenatal_wks, 
     FROM  {{ ref('stg_county_natality') }}
-    group by year
+    group by 1,2
 )
 
 select * FROM county_natality_year 
